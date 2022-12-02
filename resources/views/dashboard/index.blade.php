1c3d606300fd74a96d1b77f7b523d94dcf27e835
Gnosis <gnosis@Gnosis-MacBook-Pro.local>
2022-12-02 10:02:38 +0100

    commit chnages

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -10,25 +10,14 @@
                 <div class="col-xl-4">
                     <div class="card">
                         <div class="card-body">
-                            <div class="float-end dropdown ms-2">
-                                <a class="text-muted" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <i class="mdi mdi-dots-horizontal font-size-18"></i>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </div>
                             <div>
                                 <div class="mb-4 me-3">
                                     <i class="mdi mdi-account-circle text-primary h1"></i>
                                 </div>
 
                                 <div>
-                                    <h5>Henry Wells</h5>
-                                    <p class="text-muted mb-1">henrywells@abc.com</p>
+                                    <h5>{{ auth()->user()->name }}</h5>
+                                    <p class="text-muted mb-1">{{ auth()->user()->email }}</p>
                                     <p class="text-muted mb-0">Id no: #SK0234</p>
 
                                 </div>
